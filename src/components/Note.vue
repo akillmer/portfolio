c6721622ebca74de6cfdbc02fe3037972d8afc70
Andrew Killmer <andy@mixplate.io>
2019-11-22 07:58:15 -1000

    Initial commit

---
+++ src/components/Note.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="note">
+    <i class="far fa-sticky-note"></i>
+    <slot />
+  </p>
+</template>
+
+<style lang="scss">
+p.note {
+  font-size: 0.9em;
+  line-height: 1.5em;
+  margin-top: 2em;
+  color: #999;
+
+  i {
+    opacity: 0.5;
+  }
+
+  a {
+    color: #333;
+  }
+}
+</style>
